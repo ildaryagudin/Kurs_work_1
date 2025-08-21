5cb8e523b24ad3111a2162f6697dc2c9d0c33b60
ildaryagudin <ilyagudin70@gmail.com>
2025-08-21 16:26:18 +0500

    Создали выгодные категории кэшбэка в сервисах

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from views import main_info
-
+from services import anylize_cashback
 
 if __name__ == "__main__":
-    print(main_info("2018-05-20 15:30:00"))
+    #print(main_info("2018-05-20 15:30:00"))
+
+    result_services = anylize_cashback(file_path="../data/operations.xlsx", year=2018, month=5)
+    print(result_services)
